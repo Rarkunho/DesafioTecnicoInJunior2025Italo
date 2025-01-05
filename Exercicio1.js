3fe113cb33a21053b96aaf1a9bbf584994b64e79
Italo <2002italo.ferreira@gmail.com>
2025-01-05 20:37:40 -0300

    fix: Verifica se o numero é valido e exibe resultados em alert

--- Exercicio1.js
+++ Exercicio1.js
@@ -16,14 +16,29 @@ let b;
 let c;
 
 //Isso não funciona.
-while((typeof a) != "number"){
+while(true){
     a = parseFloat(prompt("Digite A"));
+    if(isNaN(a)){
+        alert("Digite um numero válido");
+    } else {
+        break;
+    }
 }
-while((typeof b) != "number"){
+while(true){
     b = parseFloat(prompt("Digite B"));;
+    if(isNaN(b)){
+        alert("Digite um numero válido");
+    } else {
+        break;
+    }
 }
-while((typeof c) != "number"){
+while(true){
     c = parseFloat(prompt("Digite C"));
+    if(isNaN(c)){
+        alert("Digite um numero válido");
+    } else {
+        break;
+    }
 }
 
 let delta;
@@ -38,12 +53,17 @@ if(delta > 0){
     x1 = RaizPositiva(a, b, delta);
     x2 = RaizNegativa(a, b, delta);
     console.log("Raízes Reais:\nx1: " + x1 + "  x2: " + x2);
+    //não sei se exibir no console log é o suficiente então usei o alert também.
+    alert("Delta = "+ delta + "\nRaízes Reais:\nx1: " + x1 + "  x2: " + x2);
 } else if(delta == 0){
     x1 = RaizReal(a, b);
     x2 = x1;
     console.log("Raíz Real Unica:\nx = " + x1);
+    alert("Delta = "+ delta + "\nRaíz Real Unica:\nx = " + x1);
 }else if(delta <0){
     console.log("Não há raízes reais");
+    alert("Delta = "+ delta + "\nNão há raízes reais");
 } else {
-    console.log("Delta Invalido")
+    console.log("Delta Invalido");
+    alert("Delta Inválido");
 }
